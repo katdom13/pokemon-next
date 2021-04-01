9ad6d2b59c29eb3a83107385eac961704ffe76b6
katdom13 <katdom13@gmail.com>
2021-04-02 02:48:36 +0800

    Deleted offending JSON entry

--- ssg/pages/pokemon/[name].js
+++ ssg/pages/pokemon/[name].js
@@ -5,26 +5,6 @@ import { Fragment } from 'react'
 import pokemon from '../../pokemon.json'
 
 
-export async function getStaticPaths() {
-  return {
-    paths: pokemon.map(({ name: { english } }) => ({
-      params: {
-        name: english,
-      }
-    })),
-    fallback: false
-  }
-}
-
-export async function getStaticProps(context) {
-  const data = pokemon.filter(({ name: { english } }) => english === context.params.name)[0]
-  return {
-    props: {
-      data: data,
-    }, // will be passed to the page component as props
-  }
-}
-
 export default ({ data }) => {
   // const router = useRouter()
   // const { data } = useQuery(router.query.name, getPokemon)
@@ -62,4 +42,27 @@ export default ({ data }) => {
       </Container>
     </div>
   )
+}
+
+export async function getStaticPaths() {
+
+  const paths = pokemon.map(({ name: { english } }) => ({
+    params: {
+      name: english
+    }
+  }))
+
+  return {
+    paths: paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context) {
+  const data = pokemon.filter(({ name: { english } }) => english === context.params.name)[0]
+  return {
+    props: {
+      data: data,
+    }, // will be passed to the page component as props
+  }
 }
